980553d28ddd2e4478bc7f71742d4c61f24e7255
Wellyson-Lopes <wellyson.dev@gmail.com>
2024-02-29 15:40:52 -0300

    Cria oNavbar e o Footer da página

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,49 @@
 <template>
   <div>
-    <nav>
-      <router-link to="/" src="" id="logo"></router-link>
-      <router-link to="/">Home</router-link> |
+    <div id="nav">
+      <router-link to="/" id="logo-url">
+        <img :src="logo" :alt="alt" id="logo" />
+      </router-link>
+      <router-link to="/">Home</router-link>
       <router-link to="/Pedidos">Pedidos</router-link>
-    </nav>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  props: ["logo", "alt"]
 
 }
 </script>
-<style scoped></style>
+<style scoped>
+#nav {
+  background-color: #222;
+  border-color: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+#nav #logo-url {
+  margin: auto;
+  margin-left: 0;
+}
+
+#logo {
+  width: 30px;
+  height: 30px;
+}
+
+#nav a {
+  color: #FCBA03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 5.s;
+}
+
+#nav a:hover {
+  color: #FFF
+}
+</style>
