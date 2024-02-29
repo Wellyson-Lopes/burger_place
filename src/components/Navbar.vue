96b7daecd1d44c8c77eb4f2258670a59100b8281
Wellyson-Lopes <wellyson.dev@gmail.com>
2024-02-29 14:57:16 -0300

    Add imagens e configura as rotas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/" src="" id="logo"></router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar"
+
+}
+</script>
+<style scoped></style>
